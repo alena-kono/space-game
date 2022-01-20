a6a5f4885aca74f371488166daec739583b3763b
Alena Konovalova <konovalova.alena@gmail.com>
2022-01-20 23:05:43 +0300

    Make stars to be blinking asynchronously

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import asyncio
-import time
 import curses
 import random
+import time
 
-
-TIC_TIMEOUT = 0.1
+TIC_TIMEOUT = 0.01
 STAR_SYMBOLS = "+*.:"
 
 
@@ -25,9 +24,10 @@ async def sleep_for(n_times: int):
 async def blink(canvas, row, column, symbol="*"):
     animation_states = generate_animation_states()
     while True:
-        for state, timeout in animation_states:
+        for state, _ in animation_states:
             canvas.addstr(row, column, symbol, state)
-            await sleep_for(n_times=timeout)
+            await asyncio.sleep(0)
+            time.sleep(TIC_TIMEOUT)
 
 
 def get_window_size():
@@ -59,11 +59,17 @@ def draw(canvas):
     canvas.border()
     stars_count = calculate_optimal_stars_count()
     stars = generate_random_stars(stars_count=stars_count, canvas=canvas)
-    while True:
-        for star in stars:
-            star.send(None)
+
+    for star in stars:
+        star.send(None)
         canvas.refresh()
-        time.sleep(TIC_TIMEOUT)
+        time.sleep(0)
+
+    while True:
+        random_star = random.randint(0, len(stars) - 1)
+        stars[random_star].send(None)
+        canvas.refresh()
+        time.sleep(0)
 
 
 def main():
