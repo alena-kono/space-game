03bc98e56167db737559980c4b1b2d9a7e8cd833
Alena Konovalova <konovalova.alena@gmail.com>
2022-01-20 20:21:51 +0300

    Make star count to be calculated from window size

--- main.py
+++ main.py
@@ -30,10 +30,19 @@ async def blink(canvas, row, column, symbol="*"):
             await sleep_for(n_times=timeout)
 
 
+def get_window_size():
+    return curses.initscr().getmaxyx()
+
+
 def get_random_star_coordinates():
-    window = curses.initscr()
-    height, width = window.getmaxyx()
+    height, width = get_window_size()
     return (random.randint(0, height - 1), random.randint(0, width - 1))
+
+
+def calculate_optimal_stars_count():
+    height, width = get_window_size()
+    ratio = 125
+    return int(height * width / ratio)
 
 
 def generate_random_stars(stars_count: int, canvas):
@@ -48,7 +57,8 @@ def generate_random_stars(stars_count: int, canvas):
 def draw(canvas):
     curses.curs_set(False)
     canvas.border()
-    stars = generate_random_stars(stars_count=50, canvas=canvas)
+    stars_count = calculate_optimal_stars_count()
+    stars = generate_random_stars(stars_count=stars_count, canvas=canvas)
     while True:
         for star in stars:
             star.send(None)
