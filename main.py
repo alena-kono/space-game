ec66e6a92a49dfc991bd63e7a639f8b25b202d78
Alena Konovalova <konovalova.alena@gmail.com>
2022-01-23 15:54:00 +0300

    Add one fireshot at the beginning

--- main.py
+++ main.py
@@ -21,22 +21,32 @@ async def sleep_for(n_times: int):
         await asyncio.sleep(0)
 
 
-async def blink(canvas, row, column, symbol="*"):
+async def blink(canvas, row, column, offset_tick_amount, symbol="*"):
     animation_states = generate_animation_states()
+
+    state, timeout = animation_states[0]
+    canvas.addstr(row, column, symbol, state)
+    await asyncio.sleep(0)
+
+    await sleep_for(offset_tick_amount)
     while True:
-        for state, _ in animation_states:
+        for state, timeout in animation_states:
             canvas.addstr(row, column, symbol, state)
-            await asyncio.sleep(0)
-            time.sleep(TIC_TIMEOUT)
+            await sleep_for(timeout)
 
 
 def get_window_size():
     return curses.initscr().getmaxyx()
 
 
+def get_middle_window_coordinates():
+    max_height, max_width = get_window_size()
+    return (max_height // 2, max_width // 2)
+
+
 def get_random_star_coordinates():
-    height, width = get_window_size()
-    return (random.randint(0, height - 1), random.randint(0, width - 1))
+    max_height, max_width = get_window_size()
+    return (random.randint(0, max_height - 1), random.randint(0, max_width - 1))
 
 
 def calculate_optimal_stars_count():
@@ -49,27 +59,68 @@ def generate_random_stars(stars_count: int, canvas):
     stars = []
     for _ in range(stars_count):
         row, col = get_random_star_coordinates()
-        star = blink(canvas, row, col, random.choice(STAR_SYMBOLS))
+        star = blink(
+            canvas,
+            row,
+            col,
+            offset_tick_amount=random.randint(1, 200),
+            symbol=random.choice(STAR_SYMBOLS),
+        )
         stars.append(star)
     return stars
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
 
 
 def draw(canvas):
     curses.curs_set(False)
     canvas.border()
     stars_count = calculate_optimal_stars_count()
-    stars = generate_random_stars(stars_count=stars_count, canvas=canvas)
 
-    for star in stars:
-        star.send(None)
-        canvas.refresh()
-        time.sleep(0)
+    space_objects = generate_random_stars(stars_count=stars_count, canvas=canvas)
+    space_objects.append(fire(canvas, *get_middle_window_coordinates()))
 
     while True:
-        random_star = random.randint(0, len(stars) - 1)
-        stars[random_star].send(None)
+        exhausted_coroutines = []
+        for space_object in space_objects:
+            try:
+                space_object.send(None)
+            except StopIteration:
+                exhausted_coroutines.append(space_object)
+        canvas.border()
         canvas.refresh()
-        time.sleep(0)
+        time.sleep(TIC_TIMEOUT)
+
+        for coroutine in exhausted_coroutines:
+            space_objects.remove(coroutine)
 
 
 def main():
