be45f6744555e54d295461ef337f80b0b0c91a94
Alena Konovalova <konovalova.alena@gmail.com>
2022-01-29 16:59:44 +0300

    Move fire() to spaceship module

--- space_game/main.py
+++ space_game/main.py
@@ -1,4 +1,3 @@
-import asyncio
 import curses
 import time
 from typing import Any
@@ -7,41 +6,6 @@ from space_game.settings import TIC_TIMEOUT
 from space_game.spaceship.animate import animate_spaceship, run_spaceship
 from space_game.stars.generator import (calculate_optimal_stars_count,
                                         generate_random_stars)
-
-
-async def fire(
-    canvas: Any,
-    start_row: int,
-    start_column: int,
-    rows_speed: float = -0.3,
-    columns_speed: float = 0,
-) -> None:
-    """Display animation of gun shot, direction and speed can be specified."""
-    row, column = float(start_row), float(start_column)
-
-    canvas.addstr(round(row), round(column), '*')
-    await asyncio.sleep(0)
-
-    canvas.addstr(round(row), round(column), 'O')
-    await asyncio.sleep(0)
-    canvas.addstr(round(row), round(column), ' ')
-
-    row += rows_speed
-    column += columns_speed
-
-    symbol = '-' if columns_speed else '|'
-
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 1, columns - 1
-
-    curses.beep()
-
-    while 0 < row < max_row and 0 < column < max_column:
-        canvas.addstr(round(row), round(column), symbol)
-        await asyncio.sleep(0)
-        canvas.addstr(round(row), round(column), ' ')
-        row += rows_speed
-        column += columns_speed
 
 
 def draw(canvas: Any) -> None:
--- space_game/spaceship/animate.py
+++ space_game/spaceship/animate.py
@@ -1,3 +1,5 @@
+import asyncio
+import curses
 import itertools
 from typing import Any
 
@@ -31,3 +33,38 @@ async def animate_spaceship() -> None:
     for state in itertools.cycle(animation_states):
         spaceship = state
         await sleep_for(2)
+
+
+async def fire(
+    canvas: Any,
+    start_row: int,
+    start_column: int,
+    rows_speed: float = -0.3,
+    columns_speed: float = 0,
+) -> None:
+    """Display animation of gun shot, direction and speed can be specified."""
+    row, column = float(start_row), float(start_column)
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
