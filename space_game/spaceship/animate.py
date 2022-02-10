273f457a0af709b238aabd44fd19367c9ac6c489
Alena Konovalova <konovalova.alena@gmail.com>
2022-02-11 00:06:42 +0300

    Replace single quotes to double ones

--- space_game/spaceship/animate.py
+++ space_game/spaceship/animate.py
@@ -66,17 +66,17 @@ async def fire(
     """Display animation of gun shot, direction and speed can be specified."""
     row, column = float(start_row), float(start_column)
 
-    canvas.addstr(round(row), round(column), '*')
+    canvas.addstr(round(row), round(column), "*")
     await asyncio.sleep(0)
 
-    canvas.addstr(round(row), round(column), 'O')
+    canvas.addstr(round(row), round(column), "O")
     await asyncio.sleep(0)
-    canvas.addstr(round(row), round(column), ' ')
+    canvas.addstr(round(row), round(column), " ")
 
     row += rows_speed
     column += columns_speed
 
-    symbol = '-' if columns_speed else '|'
+    symbol = "-" if columns_speed else "|"
 
     rows, columns = canvas.getmaxyx()
     max_row, max_column = rows - 1, columns - 1
@@ -86,6 +86,6 @@ async def fire(
     while 0 < row < max_row and 0 < column < max_column:
         canvas.addstr(round(row), round(column), symbol)
         await asyncio.sleep(0)
-        canvas.addstr(round(row), round(column), ' ')
+        canvas.addstr(round(row), round(column), " ")
         row += rows_speed
         column += columns_speed
